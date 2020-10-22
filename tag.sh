549a7764878006d10c1831f2f30a203ff8dc5bd2
xjm <xjm@65776.no-reply.drupal.org>
2020-10-22 12:15:26 -0500

    by xjm, catch: Use Composer to set the version in 9.1 and higher.

--- tag.sh
+++ tag.sh
@@ -12,16 +12,46 @@ function portable_sed() {
   fi
 }
 
+# @param $1
+#   The Drupal version to set.
+# @param $2
+#   The major version.
+# @param $3
+#   The minor version.
+function set_version() {
+  if [[ $2 -ge 9 && $3 -gt 0 ]] ; then
+    echo -e "\n\n Setting version with Composer for 9.1+ \n"
+    php -r "include 'vendor/autoload.php'; \Drupal\Composer\Composer::setDrupalVersion('.', '$1');"
+  else
+    grep -q "[0-9\.]*-dev" core/lib/Drupal.php
+    if [ ! $? -eq 0 ] ; then
+	echo -e "Cannot match version constant. The release must be tagged manually."
+	exit 1
+    fi
+
+    echo -e "\n\n Setting version with sed for 9.0 and earlier \n"
+    portable_sed "s/VERSION = '[0-9\.]*-dev'/VERSION = '$1'/1" "core/lib/Drupal.php"
+
+  fi
+}
+
 echo -e "Enter the release version (e.g. 8.0.6 or 8.1.0-beta2):"
 read v
 
-re="^([0-9]*)\.([0-9]*)\.([0-9]*)$"
+re="^([0-9]*)\.([0-9]*)\.([0-9]*)(-[A-Za-z0-9]*)?$"
 
 if [[ $v =~ $re ]] ; then
-  base="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}"
-  calc_n="$base.$(( ${BASH_REMATCH[3]} + 1 ))"
-  calc_p="$base.$(( ${BASH_REMATCH[3]} - 1 ))"
-  calc_b="$base.x"
+  major="${BASH_REMATCH[1]}"
+  minor="${BASH_REMATCH[2]}"
+  patch="${BASH_REMATCH[3]}"
+  suffix="${BASH_REMATCH[4]}"
+  if [ -z "$suffix" ] ; then
+    calc_n="$major.$minor.$(( $patch + 1 ))"
+    calc_p="$major.$minor.$(( $patch - 1 ))"
+  else
+    calc_n="$major.$minor.$patch"
+    calc_p="$major.$(( $minor - 1 )).0"
+  fi
   echo -e "Enter the previous release version (blank for $calc_p):"
   read p
   if [ -z "$p" ] ; then
@@ -32,35 +62,15 @@ if [[ $v =~ $re ]] ; then
   if [ -z "$n" ] ; then
     n=$calc_n
   fi
-  # Ideally we dont need this, but it's added safety.
-  # Well, it also makes the script not work for pre-release milestones, so comment it
-  # out for now.
-  # echo -e "Enter the current branch (blank for $calc_b):"
-  # read b
-  #  if [ -z "$b" ] ; then
-  #    b=$calc_b
-  #  fi
 else
-  echo -e "Enter the previous release version (e.g. 8.0.5 or 8.1.0-beta1):"
-  read p
-  echo -e "Enter the next stable release (e.g. 8.0.7 or 8.1.0):"
-  read n
-  # Ideally we dont need this, but it's added safety.
-  # echo -e "Enter the current branch (e.g. 8.8.x or 9.1.x):"
-  # read b
+  echo -e "Unrecognized version. The release must be tagged manually."
 fi
 
 echo "Composer installing."
 rm -rf vendor
 composer install --no-progress --no-suggest -n -q
 
-grep -q "[0-9\.]*-dev" core/lib/Drupal.php
-if [ ! $? -eq 0 ] ; then
-  echo -e "Cannot match version constant. The release must be tagged manually."
-  exit 1
-fi
-
-portable_sed "s/VERSION = '[0-9\.]*-dev'/VERSION = '$v'/1" "core/lib/Drupal.php"
+set_version "$v" "$major" "$minor"
 
 # Update the version strings in the metapackages
 echo "Updating metapackage versions to ${v} and tagging."
@@ -75,8 +85,8 @@ git tag -a "$v" -m "Drupal $v"
 git revert HEAD --no-edit
 
 # Put the version back to dev
-sed -i '' -e "s/VERSION = '[^']*'/VERSION = '$n-dev'/1" core/lib/Drupal.php
-echo "Restoring metapackage versions back to ${b}-dev"
+set_version "${n}-dev" "$major" "$minor"
+echo "Restoring metapackage versions back to ${n}-dev"
 
 git commit --amend -am "Back to dev."
 
